a1140cd629bb4170da8aeda9c8c84119f0cc0f50
Pavan Pophale <pawan@sdlccorp.com>
2024-04-12 17:26:28 +0530

    App Intialize

---
+++ invoice-generator/src/components/Boxes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function 
+Boxes() {
+  return (
+    <div>
+        Boxes
+    
+    </div>
+  )
+}
+
+export default 
+Boxes
---
+++ invoice-generator/src/components/MainPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './NavBar'
+
+function MainPage() {
+  return (
+    <div className='main-container'>
+         <Navbar /> 
+        <div className='sub-container'>
+            
+        </div>
+
+        
+    </div>
+  )
+}
+
+export default MainPage
