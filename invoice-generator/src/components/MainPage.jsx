35051d7f09ba269dbc3b36356191622172f73738
Harshal Mahajan <harshalmahajan@sdlccorp.com>
2024-04-15 10:21:57 +0530

    main component is created

--- invoice-generator/src/components/MainPage.jsx
+++ invoice-generator/src/components/MainPage.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import Navbar from './NavBar'
+import NameComponent from './nameComponent'
 
 function MainPage() {
   return (
-    <div className='main-container'>
-         <Navbar /> 
-        <div className='sub-container'>
-            
-        </div>
-
-        
+    <div className='main-container' style={{backgroundColor: '#fafafa', padding:'120px 0'}}>
+      <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'row'}}>
+          <div style={{backgroundColor: '#ffffff', width: '78%', height: '500px',  border: '1px solid rgb(231 234 243 / 70%)', padding: '20px', marginLeft: '50px'}}>
+            <div></div>
+            <NameComponent/>
+            <h2>Harshal</h2>
+          </div>
+          <div style={{width: '20%', height: '100%',display: 'flex', justifyContent: 'center'}}>
+            <h2>Download</h2>
+          </div>
+      </div>
+         
     </div>
   )
 }
---
+++ invoice-generator/src/components/nameComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function nameComponent() {
+  return (
+    <div>
+       <input type="password" id="oldPassword" name="oldPassword" required/>
+    </div>
+  )
+}
+
+export default nameComponent
